ce4ca26f73636d77f759852c781028dd1361d3e6
Eletronite <contato@eletronite.com.br>
2016-01-11 13:55:33 -0200

    Create BareMinimum.ino

---
+++ exemplos/01.Basics/BareMinimum.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // coloque o seu código de configuração (setup) aqui, para executar somente uma vez:
+
+}
+
+void loop() {
+  // coloque o seu código principal aqui, para executar repetidamente:
+
+}
